01fee3a685bdcc455c53c044d25d724be26ed62f
lekovicpavle13-lgtm <lekovicpavle13@gmail.com>
2026-06-08 21:13:05 +0100

    corrupted removed the code so I brang it back

--- boot.asm
+++ boot.asm
@@ -1,1 +1,22 @@
+.set ALIGN,    1<<0
+.set MEMINFO,  1<<1
+.set FLAGS,    ALIGN | MEMINFO
+.set MAGIC,    0x1BADB002
+.set CHECKSUM, -(MAGIC + FLAGS)
 
+.section .multiboot
+.long MAGIC
+.long FLAGS
+.long CHECKSUM
+
+.section .text
+.global _start
+.extern kernel_main
+
+_start:
+    cli
+    call kernel_main
+
+hang:
+    hlt
+    jmp hang
