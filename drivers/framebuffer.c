56a172e5e1b3c64082e1c1556fd24f47e2149da4
lekovicpavle13-lgtm <lekovicpavle13@gmail.com>
2026-06-09 17:19:26 +0200

    Implement framebuffer initialization and pixel manipulation

--- drivers/framebuffer.c
+++ drivers/framebuffer.c
@@ -1,1 +1,25 @@
+#include "framebuffer.h"
+
+uint32_t* framebuffer;
+uint32_t width;
+uint32_t height;
+
+void fb_init(uint32_t* fb, uint32_t w, uint32_t h)
+{
+    framebuffer = fb;
+    width = w;
+    height = h;
+}
+
+void fb_put_pixel(int x, int y, uint32_t color)
+{
+    framebuffer[y * width + x] = color;
+}
+
+void fb_clear(uint32_t color)
+{
+    for (uint32_t y = 0; y < height; y++)
+        for (uint32_t x = 0; x < width; x++)
+            fb_put_pixel(x, y, color);
+}
 
