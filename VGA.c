6e6f78aeddccbfa9a16e8ef65fce87b5a7627d24
lekovicpavle13-lgtm <lekovicpavle13@gmail.com>
2026-06-09 17:14:39 +0200

    Implement character printing and screen clearing functions
    Added functions to print characters and clear the screen in VGA.c.

--- VGA.c
+++ VGA.c
@@ -1,1 +1,35 @@
 
+#include <stdint.h>
+
+volatile uint16_t* vga = (uint16_t*)0xB8000;
+int cursor = 0;
+
+void print_char(char c)
+{
+    if (c == '\n')
+    {
+        cursor += 80 - (cursor % 80);
+        return;
+    }
+
+    if (c == '\b')
+    {
+        if (cursor > 0) cursor--;
+        vga[cursor] = (0x0F << 8) | ' ';
+        return;
+    }
+
+    vga[cursor++] = (0x0F << 8) | c;
+}
+
+void print(const char* s)
+{
+    while (*s) print_char(*s++);
+}
+
+void clear()
+{
+    for (int i = 0; i < 80 * 25; i++)
+        vga[i] = (0x0F << 8) | ' ';
+    cursor = 0;
+}
