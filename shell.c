d49aa71b7f1d9ced67dc7c624256194badbf944b
lekovicpavle13-lgtm <lekovicpavle13@gmail.com>
2026-06-09 17:04:40 +0200

    Add command handling for help, clear, and echo

--- shell.c
+++ shell.c
@@ -1,1 +1,17 @@
+void run_command(char* cmd)
+{
+    if (cmd[0]=='h' && cmd[1]=='e')
+        print("\nhelp: clear, echo\n");
+
+    else if (cmd[0]=='c')
+        clear();
+
+    else if (cmd[0]=='e')
+        print("\necho command\n");
+
+    else
+        print("\nunknown command\n");
+
+    print("\n> ");
+}
 
