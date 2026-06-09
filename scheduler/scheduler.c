d7c2d74461df37f58889099832c34e064d39a717
lekovicpavle13-lgtm <lekovicpavle13@gmail.com>
2026-06-09 17:23:12 +0200

    Implement task scheduler initialization and scheduling
    Initialize the task scheduler and implement scheduling logic.

--- scheduler/scheduler.c
+++ scheduler/scheduler.c
@@ -1,1 +1,29 @@
+#include "scheduler.h"
+
+#define MAX_TASKS 16
+
+task_t tasks[MAX_TASKS];
+int current_task = 0;
+
+void scheduler_init()
+{
+    for(int i = 0; i < MAX_TASKS; i++)
+        tasks[i].active = 0;
+}
+
+void schedule()
+{
+    current_task++;
+
+    if(current_task >= MAX_TASKS)
+        current_task = 0;
+
+    while(!tasks[current_task].active)
+    {
+        current_task++;
+
+        if(current_task >= MAX_TASKS)
+            current_task = 0;
+    }
+}
 
