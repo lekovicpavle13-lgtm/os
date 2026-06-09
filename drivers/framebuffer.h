2abcadb8b0e06568b0905ac67adeef121031cab6
lekovicpavle13-lgtm <lekovicpavle13@gmail.com>
2026-06-09 17:19:59 +0200

    Add framebuffer header with initialization and pixel functions

--- drivers/framebuffer.h
+++ drivers/framebuffer.h
@@ -1,1 +1,11 @@
+#pragma once
+#include <stdint.h>
+
+extern uint32_t* framebuffer;
+extern uint32_t width;
+extern uint32_t height;
+
+void fb_init(uint32_t* fb, uint32_t w, uint32_t h);
+void fb_put_pixel(int x, int y, uint32_t color);
+void fb_clear(uint32_t color);
 
