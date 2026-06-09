89585618bdc424eb13ac8712e5fed08fb626c72b
lekovicpavle13-lgtm <lekovicpavle13@gmail.com>
2026-06-09 16:53:04 +0200

    Add file system header with file structure and functions

--- fs/fs.h
+++ fs/fs.h
@@ -1,1 +1,19 @@
+#pragma once
+
+#define MAX_FILES 32
+#define MAX_NAME 32
+#define MAX_CONTENT 512
+
+typedef struct {
+    char name[MAX_NAME];
+    char content[MAX_CONTENT];
+    int used;
+} file_t;
+
+void fs_init();
+void fs_list();
+void fs_create(char* name);
+void fs_write(char* name, char* data);
+void fs_cat(char* name);
+void fs_delete(char* name);
 
