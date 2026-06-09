3d7680337a443ceacdf5aec80d2fbd67ac6cd09a
lekovicpavle13-lgtm <lekovicpavle13@gmail.com>
2026-06-09 16:55:53 +0200

    Add GUI header with window structure and functions

--- gui/gui.h
+++ gui/gui.h
@@ -1,1 +1,13 @@
+#pragma once
+#include <stdint.h>
+
+typedef struct window {
+    int x, y, w, h;
+    int dragging;
+    uint32_t color;
+} window_t;
+
+void gui_init();
+void gui_update();
+void gui_add_window(window_t* w);
 
