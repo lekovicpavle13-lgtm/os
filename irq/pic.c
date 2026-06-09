cd319f11fcee14fdbd26f210a004677db6b90dad
lekovicpavle13-lgtm <lekovicpavle13@gmail.com>
2026-06-09 17:22:00 +0200

    Implement PIC remapping in pic.c

--- irq/pic.c
+++ irq/pic.c
@@ -1,1 +1,27 @@
 
+#include "io.h"
+
+#define PIC1 0x20
+#define PIC2 0xA0
+#define PIC1_COMMAND PIC1
+#define PIC1_DATA (PIC1+1)
+#define PIC2_COMMAND PIC2
+#define PIC2_DATA (PIC2+1)
+
+void pic_remap()
+{
+    outb(PIC1_COMMAND, 0x11);
+    outb(PIC2_COMMAND, 0x11);
+
+    outb(PIC1_DATA, 0x20);
+    outb(PIC2_DATA, 0x28);
+
+    outb(PIC1_DATA, 0x04);
+    outb(PIC2_DATA, 0x02);
+
+    outb(PIC1_DATA, 0x01);
+    outb(PIC2_DATA, 0x01);
+
+    outb(PIC1_DATA, 0x0);
+    outb(PIC2_DATA, 0x0);
+}
