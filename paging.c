ef7490acc485d941c47bb404276ff4f6bf1f6939
lekovicpavle13-lgtm <lekovicpavle13@gmail.com>
2026-06-09 17:06:29 +0200

    Implement paging initialization function
    Initialize page directory and first page table for memory management.

--- paging.c
+++ paging.c
@@ -1,1 +1,23 @@
+#include <stdint.h>
+
+uint32_t page_directory[1024] __attribute__((aligned(4096)));
+uint32_t first_page_table[1024] __attribute__((aligned(4096)));
+
+void paging_init()
+{
+    for (int i = 0; i < 1024; i++)
+    {
+        page_directory[i] = 0x00000002; // present = 0
+        first_page_table[i] = (i * 0x1000) | 3; // present + rw
+    }
+
+    page_directory[0] = ((uint32_t)first_page_table) | 3;
+
+    asm volatile("mov %0, %%cr3" :: "r"(page_directory));
+
+    uint32_t cr0;
+    asm volatile("mov %%cr0, %0" : "=r"(cr0));
+    cr0 |= 0x80000000;
+    asm volatile("mov %0, %%cr0" :: "r"(cr0));
+}
 
