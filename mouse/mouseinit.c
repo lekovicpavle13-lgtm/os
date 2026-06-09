4040c0f74c653c1d31da0586ac6442c231b11c1f
lekovicpavle13-lgtm <lekovicpavle13@gmail.com>
2026-06-09 17:16:25 +0200

    Implement mouse initialization function

---
+++ mouse/mouseinit.c
@@ -0,0 +1,15 @@
+#include "io.h"
+
+void mouse_init()
+{
+    outb(0x64, 0xA8);
+
+    outb(0x64, 0x20);
+    uint8_t status = inb(0x60) | 2;
+
+    outb(0x64, 0x60);
+    outb(0x60, status);
+
+    outb(0x64, 0xD4);
+    outb(0x60, 0xF4);
+}
