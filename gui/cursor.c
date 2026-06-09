532bff30b7d830b5a0f0b8ca8e4a5385f45e46e1
lekovicpavle13-lgtm <lekovicpavle13@gmail.com>
2026-06-09 16:59:08 +0200

    Update cursor.c

--- gui/cursor.c
+++ gui/cursor.c
@@ -1,1 +1,8 @@
+#include "framebuffer.h"
 
+void draw_cursor(int x, int y)
+{
+    for (int i = 0; i < 8; i++)
+        for (int j = 0; j < 8; j++)
+            fb_put_pixel(x + i, y + j, 0xFFFFFF);
+}
