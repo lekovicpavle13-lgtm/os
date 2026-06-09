ffa220c84b27013d3ec4a6240948a2dad947ae00
lekovicpavle13-lgtm <lekovicpavle13@gmail.com>
2026-06-09 17:23:44 +0200

    Add task structure and scheduler function declarations

--- scheduler/scheduler.h
+++ scheduler/scheduler.h
@@ -1,1 +1,13 @@
 
+#pragma once
+#include <stdint.h>
+
+typedef struct {
+    uint32_t esp;
+    uint32_t ebp;
+    uint32_t eip;
+    int active;
+} task_t;
+
+void scheduler_init();
+void schedule();
